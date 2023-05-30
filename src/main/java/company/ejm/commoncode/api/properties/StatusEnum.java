2b845f495a3b5444e5c9cc609db2d361a2c198ac
wijoonwu <devjoon@nate.com>
2023-05-30 23:53:58 +0900

    Create StatusEnum.java

---
+++ src/main/java/company/ejm/commoncode/api/properties/StatusEnum.java
@@ -0,0 +1,17 @@
+package company.ejm.commoncode.api.properties;
+
+public enum StatusEnum {
+    CREATED(201, "CREATED"),
+    OK(200, "OK"),
+    BAD_REQUEST(400, "BAD_REQUEST"),
+    NOT_FOUND(404, "NOT_FOUND"),
+    INTERNAL_SERER_ERROR(500, "INTERNAL_SERVER_ERROR");
+
+    int statusCode;
+    String code;
+
+    StatusEnum(int statusCode, String code) {
+        this.statusCode = statusCode;
+        this.code = code;
+    }
+}
