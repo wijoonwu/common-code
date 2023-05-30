269268c87fc16db1e10a49061ba4438eb04e47e8
wijoonwu <devjoon@nate.com>
2023-05-31 01:39:13 +0900

    Create GlobalExceptionHandler.java

---
+++ src/main/java/company/ejm/commoncode/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package company.ejm.commoncode.api.exception;
+
+import company.ejm.commoncode.api.dto.ErrorDto;
+import company.ejm.commoncode.api.properties.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static company.ejm.commoncode.api.properties.ErrorCode.INTERNAL_SERVER_ERROR;
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorDto> handleCustomException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        ErrorDto errorDto = new ErrorDto(errorCode.getStatus(), errorCode.getMsg());
+        return new ResponseEntity<>(errorDto, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+    @ExceptionHandler({ Exception.class })
+    protected ResponseEntity handleServerException(Exception ex) {
+        return new ResponseEntity(new ErrorDto(INTERNAL_SERVER_ERROR.getStatus(), INTERNAL_SERVER_ERROR.getMsg()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
