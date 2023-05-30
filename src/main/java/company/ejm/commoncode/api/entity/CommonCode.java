dfc683b8c15956354efa042ff1ed056293acbd97
wijoonwu <devjoon@nate.com>
2023-05-30 23:53:53 +0900

    Create CommonCode.java

---
+++ src/main/java/company/ejm/commoncode/api/entity/CommonCode.java
@@ -0,0 +1,29 @@
+package company.ejm.commoncode.api.entity;
+
+import company.ejm.commoncode.api.dto.CommonCodeDto;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class CommonCode {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String code;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "code_group_id")
+    private CodeGroup codeGroup;
+
+    public void update(CommonCodeDto reqCodeDto) {
+        this.code = reqCodeDto.getCode();
+        this.name = reqCodeDto.getName();
+        this.codeGroup = reqCodeDto.getGroupDto().toEntity();
+    }
+}
