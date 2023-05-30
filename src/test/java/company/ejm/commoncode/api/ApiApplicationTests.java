fc5839ce5278419ee1935f292359cafd0e997a92
wijoonwu <devjoon@nate.com>
2023-05-30 21:15:48 +0900

    initial commit

---
+++ src/test/java/company/ejm/commoncode/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package company.ejm.commoncode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
